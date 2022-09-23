b1526a661fc23b1c8ffc6c9a29d5f4eccd705976
HiImJenna <skygarden316@naver.com>
2022-09-23 15:55:19 +0900

    0923

---
+++ round3/carPic.java
@@ -0,0 +1,10 @@
+public class carPic {
+    public static void main(String[] args) {
+        
+        System.out.println("      -----    ");
+        System.out.println("    /       \\");
+        System.out.println(" --           --");
+        System.out.println("|                |");
+        System.out.println(" - (   )  (   ) -   = 3");
+    }
+}
